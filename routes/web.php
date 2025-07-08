0ffc95492809c9cc48e4720fb6de3ddf603d4881
Folk160348 <passwee.da@vru.ac.th>
2025-07-08 14:29:11 +0700

    week 3

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,18 @@ require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
 
 Route::get("/homepage", function () {
-    return "<h1>This is home page Thaksinawat</h1>";
+    return "<h1>This is home page</h1>";
 });
 
 Route::get("/blog/{id}", function ($id) {
     return "<h1>This is blog page : {$id} </h1>";
 });
+
+Route::get("/blog/{id}/edit", function ($id) {
+    return "<h1>This is blog page : {$id} for edit</h1>";
+});
+
+//routes/web.php
 
 Route::get("/hello", function () {
     return view("hello");
@@ -30,22 +36,24 @@ Route::get("/hello", function () {
 
 Route::get('/greeting', function () {
 
-    $name = 'James';
-    $last_name = 'Mars';
+    $name = 'Mark';
+    $last_name = 'Suckerberge';
 
     return view('greeting', compact('name', 'last_name'));
 });
+// quiz 1//
 
+Route::get( "/gallery" , function(){
+	$ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
+$bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg"; 
+$cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
+$god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg"; 
+$spider = "https://blog.richersounds.com/wp-content/uploads/2019/07/spider-man-768x384.jpg"; 
 
-Route::get("/gallery", function () {
-    $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
-    $bird = "https://images.indianexpress.com/2021/03/falcon-anthony-mackie-1200.jpg";
-    $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
-    $god = "https://www.blackoutx.com/wp-content/uploads/2021/04/Thor.jpg";
-    $spider = "https://icdn5.digitaltrends.com/image/spiderman-far-from-home-poster-2-720x720.jpg";
-
-    return view("test/index", compact("ant", "bird", "cat", "god", "spider"));
+return view("test/index", compact("ant","bird","cat","god","spider") );
 });
+
+// quiz 2 //
 
 Route::get("/gallery/ant", function () {
     $ant = "https://cdn3.movieweb.com/i/article/Oi0Q2edcVVhs4p1UivwyyseezFkHsq/1107:50/Ant-Man-3-Talks-Michael-Douglas-Update.jpg";
@@ -62,19 +70,37 @@ Route::get("/gallery/cat", function () {
     return view("test/cat", compact("cat"));
 });
 
-
-Route::get("/teacher", function () {
-    return view("teacher");
+Route::get("/teacher" , function (){
+	return view("teacher");
 });
 
-Route::get("/student", function () {
-    return view("student");
+Route::get("/student" , function (){
+	return view("student");
 });
 
-Route::get("/theme", function () {
-    return view("theme");
+Route::get("/theme" , function (){
+	return view("theme");
 });
 
 Route::get('/active/index', function () {
     return view('active/index');
 })->name('index');
+
+Route::get('/active/about', function () {
+    return view('active/about');
+})->name('about');
+Route::get('/active/services', function () {
+    return view('active/services');
+})->name('services');
+Route::get('/active/portfolio', function () {
+    return view('active/portfolio');
+})->name('portfolio');
+Route::get('/active/team', function () {
+    return view('active/team');
+})->name('team');
+Route::get('/active/blog', function () {
+    return view('active/blog');
+})->name('blog');
+Route::get('/active/contact', function () {
+    return view('active/contact');
+})->name('contact');
