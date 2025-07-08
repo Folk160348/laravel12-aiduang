60f80e8b95b05c544cf10c219c027059daaf99a8
Folk160348 <passwee.da@vru.ac.th>
2025-07-08 14:26:44 +0700

    Initial commit

---
+++ resources/views/test/ant.blade.php
@@ -0,0 +1,14 @@
+<!-- View stored in resources/views/greeting.blade.php -->
+<html>
+
+<body>
+    <h1>Gallery of Thaksinawat 67122420019 </h1>
+
+     <a href="gallery/ant" ><img src="{{$ant}}" alt="Trulli" width="500" height="333"></a>
+    
+
+
+
+</body>
+
+</html>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,17 @@
+<!-- View stored in resources/views/greeting.blade.php -->
+<html>
+
+<body>
+    <h1>Gallery of Thaksinawat 67122420019 </h1>
+
+     <a href="gallery/ant" ><img src="{{$ant}}" alt="Trulli" width="500" height="333"></a>
+     <a href="gallery/ant" ><img src="{{$bird}}" alt="Trulli" width="500" height="333"></a>
+     <a href="gallery/ant" ><img src="{{$cat}}" alt="Trulli" width="500" height="333"></a>
+     <a href="gallery/ant" ><img src="{{$god}}" alt="Trulli" width="500" height="333"></a>
+     <a href="gallery/ant" ><img src="{{$spider}}" alt="Trulli" width="500" height="333"></a>
+
+
+
+</body>
+
+</html>
